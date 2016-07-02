0639739a717aba905a959aea6ff7258f9a0edbf1
moogar0880 <moogar0880@gmail.com>
2016-07-02 16:04:55 -0400

    fix dependency installation

--- itunes/__init__.py
+++ itunes/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """A python interface for searching the iTunes Store"""
 
-version_info = (1, 0, 0)
+version_info = (1, 0, 1)
 __name__ = 'itunes'
 __doc__ = 'A python interface to search iTunes Store'
 __author__ = 'Jonathan Nappi'
--- setup.py
+++ setup.py
@@ -2,10 +2,16 @@
 import itunes
 from setuptools import setup
 
+with open('README.rst') as f:
+    readme = f.read()
+with open('requirements.txt') as f:
+    requires = [line.strip() for line in f if line.strip()]
+
 setup(
     name='pyitunes',
     version=itunes.__version__,
     description="A simple python wrapper to access iTunes Store API",
+    long_description=readme,
     author='Jonathan Nappi',
     author_email='moogar@comcast.net',
     maintainer='Jonathan Nappi',
@@ -14,4 +20,6 @@ setup(
     platforms=['any'],
     url='https://github.com/moogar0880/python-itunes',
     packages=['itunes'],
+    install_requires=requires,
+    zip_safe=False,
 )
