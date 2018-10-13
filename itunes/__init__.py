3e6970f54f50bddc9a09bf1527db7dade39998f8
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2018-10-12 23:44:26 -0700

    Update author data

--- itunes/__init__.py
+++ itunes/__init__.py
@@ -4,11 +4,11 @@
 version_info = (1, 0, 1)
 __name__ = 'itunes'
 __doc__ = 'A python interface to search iTunes Store'
-__author__ = 'Jonathan Nappi'
+__author__ = ['Jonathan Nappi', 'Oscar Celma', 'Vinyl Darkscratch']
 __version__ = '.'.join([str(i) for i in version_info])
 __license__ = 'GPL'
 __maintainer__ = 'Jonathan Nappi'
-__email__ = 'moogar@comcast.net'
+__email__ = ['moogar@comcast.net', 'ocelma@bmat.com', 'vinyldarkscratch@gooborg.com']
 __status__ = 'Stable'
 
 #: iTunes API version
