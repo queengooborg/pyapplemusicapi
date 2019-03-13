a048af362b9b9a4aaf1720017cac94ae769e3dec
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2019-03-13 05:18:36 -0700

    Bump version number

--- itunes/__init__.py
+++ itunes/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """A python interface for searching the iTunes Store"""
 
-version_info = (1, 1, 6)
+version_info = (1, 1, 7)
 __name__ = 'itunes'
 __doc__ = 'A python interface to search iTunes Store'
 __author__ = ['Vinyl Darkscratch', 'Jonathan Nappi', 'Oscar Celma']
